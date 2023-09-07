bf8409a52a59c60fff8ce89b2df927264aab95be
Abdel-rahmanalaa <abdellrahmanalaa@gmail.com>
2023-09-07 11:32:16 +0200

    MarvelFlix App

---
+++ android/app/src/main/kotlin/com/example/marvelflix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.marvelflix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:marvelflix/src/app/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
