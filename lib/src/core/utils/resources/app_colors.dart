f70f03397c2f6bb3980e41cfc99e4cad9b396657
Abdel-rahmanalaa <abdellrahmanalaa@gmail.com>
2023-09-07 13:18:24 +0200

    update

---
+++ lib/src/core/utils/resources/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkBlue = Color(0xFF110E47);
+  static const Color lightBlue = Color(0xFF0098FF);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color lightGrey = Color(0xFF777777);
+  static const Color lightGrey2 = Color(0xFFAAA9B1);
+  static const Color lightGre3 = Color(0xFF9C9C9C);
+  static const Color transparent = Colors.transparent;
+}
---
+++ lib/src/core/utils/resources/app_strings.dart
@@ -0,0 +1,52 @@
+class AppStrings {
+  static const String emptySpace = ' ';
+
+  //Register Screens
+  static const String hey = 'Hey, Hello';
+  static const String enterYourAccount =
+      'Enter your credentials to access your account';
+  static const String name = 'Name';
+  static const String emailadrress = 'Email address';
+  static const String password = 'Password';
+  static const String agreeTermsAndPrivacy = 'I agree to the Terms & Privacy';
+  static const String signUp = 'Sign up';
+  static const String alreadyHaveAccount = 'Already have an account?';
+
+  //Login Screen
+  static const String letsConnectWithUs = 'Let\'s Connect With Us!';
+  static const String emailAddress = 'Email Address';
+  static const String forgotPassword = 'Forgot Password?';
+  static const String login = 'Login';
+  static const String or = 'Or';
+  static const String dontHaveAccount = 'Don\'t have an account?';
+  static const String register = 'Register';
+
+//Home Screen
+  static const String marvel = 'Marvel';
+  static const String tvShows = 'Tv Shows';
+  static const String movies = 'Movies';
+  static const String seeMore = 'See more';
+  static const String abWorkout = 'Ab Workout';
+
+  //Details Screen
+  static const String description = 'Description';
+  static const String relatedMovies = 'Related Movies';
+  static const String length = 'Length';
+  static const String director = 'Director';
+  static const String phase = 'Phase';
+  static const String season = 'Season';
+  static const String episodes = 'Episodes';
+ 
+}
+
+class AppPharses {
+  String phraseConstruction(String firstString, String secondString) {
+    return firstString + secondString;
+  }
+
+  String phraseConstructionWithFirstStringEmpty(String secondString) {
+    return AppStrings.emptySpace + secondString;
+  }
+
+  phraseConsphraseConstructionWithFirstStringEmptyruction(secondString) {}
+}
