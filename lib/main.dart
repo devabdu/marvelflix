726a5d3d2f02ee8e56e57e141198fdb12963f3a4
Abdel-rahmanalaa <abdellrahmanalaa@gmail.com>
2023-09-09 06:26:56 +0200

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:marvelflix/firebase_options.dart';
 import 'package:marvelflix/src/app/my_app.dart';
 import 'package:marvelflix/src/app/services_locator.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   ServicesLocator().init();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
