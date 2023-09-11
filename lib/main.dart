be10626b7233a190ecb2b1092f2524357f318e15
Abdel-rahmanalaa <abdellrahmanalaa@gmail.com>
2023-09-11 09:05:36 +0200

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:marvelflix/firebase_options.dart';
 import 'package:marvelflix/src/app/my_app.dart';
 import 'package:marvelflix/src/app/services_locator.dart';
+import 'package:marvelflix/src/config/router/app_routes.dart';
+
+late String initialRoute;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,6 +15,8 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  FirebaseAuth.instance.authStateChanges().listen((user) => user == null
+      ? initialRoute = AppRoutesName.registerRoute
+      : initialRoute = AppRoutesName.homeRoute);
   runApp(const MyApp());
 }
